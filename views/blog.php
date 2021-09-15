6d554be4ec54a210e697444f72eaa9cd82aa39d3
Dmytro IOSYPENKO <d.iosypenko@gmail.com>
2021-09-15 10:30:34 +0200

    added reset.css, reworked pages

--- views/blog.php
+++ views/blog.php
@@ -1,25 +1,17 @@
-<?php
+    <?php
+    //Variable avec le titre de la page;
+    $title = "Blog";
+    //Appel template header
+    require_once("./templates/header.php");
+    ?>
 
-    require_once('../src/controllers/UserController.php'); 
+    <main>
+        <section>
 
-    if(!empty($_GET['mail']) && !empty($_GET['pass'])){
-        $userController = new UserController(NULL, $_GET['pass'], NULL, NULL, $_GET['mail'], NULL, NULL);
-        $userController->login();
-    }
-
-    require_once('./templates/header.php');
-?>
-
-    <section>
-        <form action="login.php" method="get">
-            <label for="mail">Email</label>
-            <input type="email" id="mail" name="mail">
-
-            <label for="pass">Mot de passe</label>
-            <input type="password" id="pass" name="pass" minlength="5">
-
-            <button>Se connecter</button>
-        </form>
-    </section>
-    </body>
-</html>
+        </section>
+    </main>
+    
+    <?php
+    //Appel template footer
+    require_once("./templates/footer.php");
+    ?>
---
+++ views/sites_amis.php
@@ -0,0 +1,17 @@
+    <?php
+    //Variable avec le titre de la page
+    $title = "Sites amis";
+    //Appel template header
+    require_once("./templates/header.php");
+    ?>
+
+    <main>
+        <section>
+
+        </section>
+    </main>
+
+    <?php
+    //Appel template footer
+    require_once("./templates/footer.php");
+    ?>
