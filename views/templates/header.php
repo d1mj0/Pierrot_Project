96a0e3bf6f66aa7edef7a8319ce63ce11c3d989e
Xeuhma! <xhm1@protonmail.com>
2021-09-15 19:36:28 +0200

    update css/burger/index

--- views/templates/header.php
+++ views/templates/header.php
@@ -9,7 +9,9 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="../views/assets/js/script.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <title>
         <?php
             //Affiche le titre de la page
@@ -24,15 +26,26 @@
     </div>
 
         
-        <nav>
-            <div class="nav-container">
-                <div class="row">
-                    <div class="nav-link_menu col-sm10 col-md-10  col-lg-2 mx-auto"><a href="index.php">Accueil</a></div>
-                    <div class="nav-link_menu col-sm10 col-md-10  col-lg-2 mx-auto"><a href="pensee_globale.php">Pensée globale</a></div>
-                    <div class="nav-link_menu col-sm10 col-md-10  col-lg-2 mx-auto"><a href="a_propos.php">&#xC0; propos</a></div> 
-                    <div class="nav-link_menu col-sm10 col-md-10  col-lg-2 mx-auto"><a href="travaux_et_publications.php">Travaux et publications</a></div>
-                </div>
-            </div>
+        <nav class="nav-container navbar-expand-md navbar-light">
+            <button class="navbar-toggler align-center" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link_menu col-lg-2 mx-auto" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link_menu col-lg-2 mx-auto" href="pensee_globale.php">Pensée globale<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link_menu col-lg-2 mx-auto" href="a_propos.php">&#xC0; propos</a> 
+                    </li>
+                    <li class="nav-item">    
+                        <a class="nav-link_menu col-lg-2 mx-auto" href="travaux_et_publications.php">Travaux et publications</a>
+                    </li>
+                </ul>
+            
         </nav>
         
     </header>
