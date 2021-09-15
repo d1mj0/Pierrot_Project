764ae4da8e0e8a2b95ebd490c817acb9c032dfa8
Xeuhma! <xhm1@protonmail.com>
2021-09-15 11:43:48 +0200

    update header/apropos/login/css

--- views/backoffice/login.php
+++ views/backoffice/login.php
@@ -9,17 +9,40 @@
 
     require_once('./templates/header.php');
 ?>
-
-    <section>
-        <form action="login.php" method="get">
-            <label for="mail">Email</label>
-            <input type="email" id="mail" name="mail">
-
-            <label for="pass">Mot de passe</label>
-            <input type="password" id="pass" name="pass" minlength="5">
-
-            <button>Se connecter</button>
-        </form>
-    </section>
-    </body>
+<body>
+    <main>
+        <div id="layoutAuthentication">
+                <div id="layoutAuthentication_content">
+                    <main>
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-lg-5">
+                                    <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                        <div class="card-header"><h3 class="text-center font-weight-light my-4">Connection</h3></div>
+                                        <div class="card-body">
+                                            <form>
+                                                <div class="form-floating mb-3">
+                                                    <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                                    <label for="inputEmail">Email address</label>
+                                                </div>
+                                                <div class="form-floating mb-3">
+                                                    <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
+                                                    <label for="inputPassword">Password</label>
+                                                </div>
+                                                <div class="form-check mb-3">
+                                                    <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
+                                                    <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
+                                                </div>
+                                                <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                        <a class="btn btn-primary" href="blog_administration.php">Login</a>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+    </main>
+</body>
 </html>
