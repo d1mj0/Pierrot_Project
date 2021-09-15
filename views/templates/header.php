9ba43c56a01529160d40bd3d948618d7f1e63491
Dmytro IOSYPENKO <d.iosypenko@gmail.com>
2021-09-15 13:19:48 +0200

    update

--- views/templates/header.php
+++ views/templates/header.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/reset.css">
-    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="../views/assets/css/reset.css">
+    <link rel="stylesheet" href="../views/assets/css/style.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="../assets/js/script.js"></script>
+    <script src="../views/assets/js/script.js"></script>
     <title>
         <?php
             //Affiche le titre de la page
@@ -23,7 +23,7 @@
                 <ul class="row">
                     <li class="nav-link_menu-col-sm10-md-10-mx-auto-lg-2"><a href="index.php">Accueil</a></li>
                     <li class="nav-link_menu-col-sm10-md-10-mx-auto-lg-2"><a href="pensee_globale.php">Pensée globale</a></li>
-                    <li class="nav-link_menu-col-sm10-md-10-mx-auto-lg-2"><a href="a_propos.php">à propos</a></li> 
+                    <li class="nav-link_menu-col-sm10-md-10-mx-auto-lg-2"><a href="a_propos.php">&#xC0; propos</a></li> 
                     <li class="nav-link_menu-col-sm10-md-10-mx-auto-lg-2"><a href="travaux_et_publications.php">Travaux et publications</a></li>
                 </ul>
             </div>
