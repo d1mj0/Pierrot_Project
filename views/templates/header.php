85778e261a1284d2421758f500e3f7280e16525b
Xeuhma! <xhm1@protonmail.com>
2021-09-15 09:05:26 +0200

    header

--- views/templates/header.php
+++ views/templates/header.php
@@ -9,26 +9,15 @@
 </head>
 <body>
     <header>
+
         <nav>
             <ul>
                 <li class="link_menu"><a href="index.php">Accueil</a></li>
-                <?php if(empty($_SESSION['user'])): ?>
-                    <li class="link_menu"><a href="login.php">Connexion</a></li>
-                    <?php else: ?>
-                    <li class="link_menu"><a href="profil.php">Profil</a></li>
-                    <li class="link_menu"><a href="deconnexion.php">Déconnexion</a></li>
-                <?php endif; ?>
+                <li class="link_menu"><a href="pensee_globale.php">Pensée globale</a></li>
+                <li class="link_menu"><a href="a_propos">à propos</a></li> 
+                <li class="link_menu"><a href="travaux_et_publications.php">Travaux et publications</a></li>
             </ul>
         </nav>
-        <?php if(!empty($_SESSION) && $_SESSION['user']['rank'] === "admin"): ?>
-        <nav>
-            <ul>
-                <li class="link_menu"><a href="_administration.php">Agences</a></li>
-                <li class="link_menu"><a href="command_administration.php">Commandes</a></li>
-                <li class="link_menu"><a href="user_administration.php">Membres</a></li>
-                <li class="link_menu"><a href="car_administration.php">Vehicules</a></li>
-            </ul>
-        </nav>
-        <?php endif; ?>
+        
     </header>
     
