a44b686729bcbca35a57380e0965a0629c9526e7
Xeuhma! <xhm1@protonmail.com>
2021-10-04 16:41:52 +0200

    update pensee globale

--- views/pensee_globale.php
+++ views/pensee_globale.php
@@ -9,41 +9,71 @@
         <br>
         <section>
             <div class="container-fluid">
-                    <div class="row">
-                        <div class="title col-12">
-                            <h2>Pensée Globale</h2>
-                        </div>
+                <div class="row">
+                    <div class="title col-12">
+                        <h2>Pensée Globale</h2>
                     </div>
-                    <div class="row">
-                        <div class="col-4">
-                            
-                        </div>
-                        <div class="col-5">
-                            <img src="../views/assets/img/de l'eversion title header.svg" alt="">
-                        </div>
-                        <div class="col-4">
-                            
-                        </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-4">
                     </div>
-                    <div class="row">
-                        <div class="col-4">
-                            
-                        </div>
-                        <div class="col-4 mx auto">
-                            <img src="../views/assets/img/Eversion_370px.svg" alt="Représentation graphique de l'éversion">
-                        </div>
-                        <div class="col-4">
-                            
-                        </div>
+                    <div class="col-5">
+                        <img src="../views/assets/img/de l'eversion title header.svg" alt="">
                     </div>
-                        <div class="col-12">
-                            <p>Pour cela, je ne vois qu’une seule solution, en finir avec la hiérarchie. Par hiérarchie, j’entends de cette pensée distinctive, expression de la pensée duale de l’ordre théocratique. Mais on ne vient pas à bout de la hiérarchie simplement par marotte idéologique ou juste en l’énonçant de façon performative. De fait on ne vient pas à bout de la hiérarchie, ou si l’on y parvient c’est en la dépassant. On ne peut la détruire, elle est beaucoup trop complexe pour cela. En revanche, nous pouvons la dépasser en perforant sa complexité. Or il ne fait aucun doute pour moi que la perforation de la complexité hiérarchique sonnera l’avènement de l’anarchie. Si la hiérarchie est l’expression parfaite de l’ordre, il existe une plus parfaite configuration que l’ordre, c’est l’harmonie. Or la plus que parfaite expression de l’harmonie n’est autre que l’anarchie. Pour s’en convaincre il suffit simplement de s’en remettre aux suffixes grecs que sont « kata » et « ana ». La hiérarchie est une « kata-archie », un ordonnancement du haut vers le bas, principe même de toute hiérarchie. 
-L’anarchie quant à elle n’est pas ou est moins une absence d’ordonnancement, une « an-archie » qu’une « ana-archie », soit un ordonnancement du bas vers le haut, principe de l’anarchie organisationnelle pour laquelle les mandatés ne sont là que pour exécuter les décisions prise par la base. Mais au-delà de cette précision étymologique d’ordre sémantique, le point fondamental à retenir est qu’en grec ancien, le ana est un principe supérieur au kata. Un principe supérieur car hlfasf. Il l’est tout tant dans l’expression conceptuelle. Ainsi au kata correspond la transcendance tandis qu’ana se réfère à l’immanence. Or si l’on en juge une nouvelle fois par l’étymologie, « transcendance » du grec est un piège placé sur le chemin. Un piège placé sur le chemin de l’entendement, alors que l’immanence est poétiquement un principe de justice contenu dans les causes elle-même et le cours “naturel” des conséquences, le cheminement même de l’entendement. 
-Ainsi, si la hiérarchie est la parfaite expression de l’ordre holistique, l’anarchie est la plus que parfaite expression de l’harmonie systémique. Plus simplement dit, la hiérarchie est au holisme, ce que l’anarchie est au système. Mais le système est plus que le Tout, car un système est un ensemble de Touts. Mais, bien entendu, pour comprendre un système, il faut déjà comprendre ce qui le compose, à savoir comprendre ce qu’est un Tout et surtout comment il “fonctionne”. Mais lorsqu’il sera admis, et c’est tout l’enjeu de cette réflexion, qu’un Tout fonctionne par inversion, alors il pourra être accepter qu’un système comme « ensemble de Touts » fonctionne par inversion d’inversion soit « éversion ». Si l’inversion d’une inversion est une éversion, l’éversion n’est pas neutre et moins encore inoffensive. L’éversion est un processus entropique, elle est le principe du chaos. Autant dire qu’il ne fait pas l’ombre d’un doute pour moi que l’éversion est le principe même de l’univers. 
-Autant dire également qu’établir formellement le processus d’éversion ne consiste en rien d’autre qu’ouvrir un dialogue avec l’univers. Le défi, pour ne pas dire la gageure, que nous souhaitons relever ici ne consiste en rien de moins que de tenter d’établir un dialogue avec l’univers en établissant une équation du langage. Si nous parlons tous un langage, le langage n’est pas en reste et nous parle en retour. Il nous parle à travers les langues d’hier et d’aujourd’hui comme d’ici est d’ailleurs, entre celles d’hier et d’ailleurs et celles d’ici et d’aujourd’hui comme entre celles d’ailleurs aujourd’hui et d’hier ici. Et il nous parle à travers toutes ces langues par éversion. S’il peut ainsi nous parler c’est que surtout nous sommes parlés par le langage, par ce langage qu’est l’éversion. Ainsi la compréhension de l’éversion nous laisse caresser l’espoir d’une communication avec l’univers comme la possibilité d’entrevoir sa compréhension. Et qui sait, peut-être que ce langage se parle à lui-même, que l’univers se parle à lui-même à travers nous ? Ce qui ferait de nous des médiateurs bien privilégiés.</p>
-                        </div>
+                    <div class="col-4">
                     </div>
-                </div>                             
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <p class="text">
+                        Le yin (noir) et le yang (blanc) symbolisent les deux catégories complémentaires de la phénoménologie chinoise 
+                        antique pour qualifier l’ambivalence de la dualité. Alors que leur forme en « S » signifie leur mouvement 
+                        continuel, les points dans les deux symboles <em>yin</em> et <em>yang</em> correspondent au fait que dans l’existence rien ne 
+                        serait absolu. Tout comme nul n’est absolument sensé ignorer que dans la vie « rien n’est ou tout blanc ou 
+                        tout noir… ». 
+                        La volonté d’y apporter quelques nuances de gris repose donc sur cette simple idée que <strong>si rien n’est ou tout noir ou tout blanc</strong>, 
+                        alors autant dire que <strong>tout oscille entre le gris foncé et le gris clair</strong>. Mais relativiser ainsi cette conception dualiste manichéenne 
+                        n’est pas neutre. Relativiser le <em>yin-yang</em> c’est pointer que derrière la classification binaire se tapit un authentique système 
+                        hiérarchique. Relativiser le yin-yang en le complexifiant est tout l’enjeu de notre propos : le dépassement de la hiérarchie 
+                        (par elle-même). 
+                        </p>   
+                    </div>
+                    <div class="col-lg-4 mx-auto">
+                        <img src="../views/assets/img/Eversion_370px.svg" alt="Représentation graphique de l'éversion">
+                    </div>
+                    <div class="col-lg-4">
+                        <p class="text">
+                        Et pour cela, il n’y a qu’à observer. 
+                        Ainsi, lorsqu’on s’y 
+                        attarde un peu on remarque qu’apparaissent 
+                        schématiquement « quatre dauphins ».
+                        Quatre figures imbriquées qui sont moins à considérer pour ce 
+                        qu’elles représentent que pour ce qu’elles signifient. Étymologiquement, le mot <strong>dauphin</strong> renvoie à celui
+                        d’« utérus [matrice] ». Or telle est bien l’illustration du processus d’Éversion. L’éversion est un 
+                        processus qui combine quatre matrices. L’éversion est donc un processus matriciel analogique. 
+                        Analogiquement, la philosophie antique chinoise considère le symbole yin-yang comme celui de la complémentarité des oppositions.
+                        La complexité apportée par nos soins, celle opérée par les nuances de gris que sont le <em>gris clair</em> et le <em>gris foncé</em>s, illustre elle, 
+                        une complémentarité entre « opposition » et « contrariété ». Dans la perspective proposée, cette complémentarité ne relève dès lors
+                        plus d’une opposition mais d’une inversion. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                    <figure class="figureb p-3 rounded">
+                        <blockquote  class="blockquote mb-4 text-center"><p>Tel est l’axiome de l’<strong>Éversion</strong> :<br> celui de la complémentarité des <br><strong>inverses</strong>.</p></blockquote>
+                    </figure>
+                    </div>
+                </div>
+            </div>                           
         </section>
         <br>
         <br>
