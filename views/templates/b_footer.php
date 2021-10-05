2b153946b6b92386ecdb3bd77efd788c54afaf0f
Xeuhma! <xhm1@protonmail.com>
2021-10-05 18:59:33 +0200

    marre marre marre

--- views/templates/b_footer.php
+++ views/templates/b_footer.php
@@ -9,7 +9,6 @@
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-        <script src="js/datatables-simple-demo.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.3/datatables.min.js"></script>
     </body>
 </html>
