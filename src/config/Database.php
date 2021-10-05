2399c92f23a8005b11d5741f591aa2d285b89845
d1mj0 <d.iosypenko@gmail.com>
2021-10-05 16:30:35 +0200

    newsletter

--- src/config/Database.php
+++ src/config/Database.php
@@ -1,25 +1,16 @@
 <?php
+    class Database{
+        private $host = "localhost"; 
+        private $dbname = "eversiondb2"; 
+        private $username = "admin_eversion";
+        private $pass = "De1ever5!0n";
 
-class Database{
-
-    private $host = "localhost"; 
-    
-    private $dbname = "eversiondb2"; 
-
-    private $username = "root";
-
-    private $pass = "";
-
-    public function getPDO(){
-        try{
-            return new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username, $this->pass);
-        } catch(PDOException $error){
-            exit($error);
+        public function getPDO(){
+            try{
+                return new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username, $this->pass);
+            } catch(PDOException $error){
+                exit($error);
+            }
         }
     }
-
-}
-
-
-
 ?>
--- views/newsletter.php
+++ views/newsletter.php
@@ -3,53 +3,10 @@
     $title = "Newsletter";
     //Appel template header
     require_once('./templates/header.php');
-?>
-
-<main>
-    <div id="layoutContact">
-        <div id="layoutContact_content">
-            <section>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-5">
-                                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Inscription newsletter</h3></div>
-                                        <div class="card-body">
-                                            <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                                                <div class="form-floating mb-3">
-                                                    <input class="form-control" id="email" type="email" placeholder="name@example.com" name="email" required value=""/>
-                                                    <label for="email">Email <span class="err"><?php echo $emailErr; ?></span></label>
-                                                </div>
-                                                <div class="form-floating mb-3">
-                                                    <input class="form-control" id="lastname" type="text" placeholder="Nom" name="lastname" pattern="^[a-zA-Z-' ]*$" required value="<?php echo $lastname; ?>"/>
-                                                    <label for="lastname">Nom <span class="err"><?php echo $lastnameErr; ?></span></label>
-                                                </div>
-                                                <div class="form-floating mb-3">
-                                                    <input class="form-control" id="name" type="text" placeholder="Prénom" pattern="^[a-zA-Z-' ]*$" name="name" required value="<?php echo $name; ?>"/>
-                                                    <label for="name">Prénom <span class="err"><?php echo $nameErr; ?></span></label>
-                                                </div>
-                                                <div class="d-flex align-items-center justify-content-center mt-4 mb-0">
-                                                    <input type=submit class="btn" id="submit" value="Envoyer"/>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            </section>
-        </div>
-    </div>
-    <br>
-    <br>
-</main>
-
-<?php
 
     require_once("../src/controllers/NewsletterController.php");
     //Validation du formulaire côté serveur
-
+/*
     $email = $lastname = $name = "";
     $emailErr = $lastnameErr = $nameErr = "";
 
@@ -89,10 +46,56 @@
         $data = htmlspecialchars($data);
         return $data;
     }
+*/
+    $name = $_POST['name'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
 
-    $newsletterController = new NewsletterController($_POST['lastname'], $_POST['name'], $_POST['email']);
+    $newsletterController = new NewsletterController($name, $lastname, $email);
     $newsletterController->addEmail();
+
+    var_dump($newsletterController);
 ?>
+
+<main>
+    <div id="layoutContact">
+        <div id="layoutContact_content">
+            <section>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Inscription newsletter</h3></div>
+                                        <div class="card-body">
+                                            <form method="POST" enctype="multipart/form-data" action="newsletter.php">
+                                                <div class="form-floating mb-3">
+                                                    <input class="form-control" id="email" type="email" placeholder="name@example.com" name="email" required value=""/>
+                                                    <label for="email">Email <span class="err"><?php echo $emailErr; ?></span></label>
+                                                </div>
+                                                <div class="form-floating mb-3">
+                                                    <input class="form-control" id="lastname" type="text" placeholder="Nom" name="lastname" pattern="^[a-zA-Z-' ]*$" required value="<?php echo $lastname; ?>"/>
+                                                    <label for="lastname">Nom <span class="err"><?php echo $lastnameErr; ?></span></label>
+                                                </div>
+                                                <div class="form-floating mb-3">
+                                                    <input class="form-control" id="name" type="text" placeholder="Prénom" pattern="^[a-zA-Z-' ]*$" name="name" required value="<?php echo $name; ?>"/>
+                                                    <label for="name">Prénom <span class="err"><?php echo $nameErr; ?></span></label>
+                                                </div>
+                                                <div class="d-flex align-items-center justify-content-center mt-4 mb-0">
+                                                    <input type="submit" class="btn" id="submit" value="Envoyer"/>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </section>
+        </div>
+    </div>
+    <br>
+    <br>
+</main>
 
 <?php
     //Appel template footer
