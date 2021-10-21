b45dfe56dfbea4bd1f9e194b65c840f1cf6e129d
Xeuhma! <xhm1@protonmail.com>
2021-10-22 01:00:46 +0200

    delete/views article

--- views/b_delete_article.php
+++ views/b_delete_article.php
@@ -3,13 +3,16 @@ $title =  "Suppression d'article";
 
 require_once('../views/templates/b_header.php'); 
 require_once('../src/models/ArticleModel.php'); 
+require_once('../src/models/ArticleModel.php'); 
 
+$id= 0;
 
-
+$articles = new ArticleModel;
+$articleR = $articles->getOne($_GET['id']);
 
 
 if(isset($_POST['deleteArticle'])){
-$id = $_POST['idArticle'];
+    $id = $_POST['idArticle'];
 
 $articleDelete = new ArticleModel;
 $articleD = $articleDelete->delete($id);
@@ -27,8 +30,32 @@ echo '<div class="alert alert-danger>
 </div>';
 }
 }
-
 ?>
+    <section class="text-center">
+        <br>
+        <br>
+        <h2 class="mb-5">Voulez-vous vraiment supprimer cette article?</h2>
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 col-md-12">
+                        <form action="b_delete_article.php?<?= $_GET['id']; ?>" method="POST">   
+                            <input type="hidden" name="idArticle" value="<?= $_GET['id']; ?>">
+                            <div class="card m-4 ">
+                                <div class="bg-image">
+                                    <img class="img-fluid" src="<?= $articleR[0]['img']; ?>" alt="">
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title"><?= $articleR[0]['title']; ?></h5> 
+                                </div>
+                                <div>
+                                    <input type="submit"  name="deleteArticle"  value="delete">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </section>
 <?php
     require_once('../views/templates/b_footer.php');
 ?>
---
+++ views/b_view_article.php
@@ -0,0 +1,61 @@
+<?php
+$title =  "Mise à jour de l'article";
+require_once('../views/templates/b_header.php'); 
+require_once('../src/models/ArticleModel.php'); 
+
+
+$articles = new ArticleModel;
+$articleR = $articles->getOne($_GET['id']);
+
+setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
+    $d= $articleR[0]['date_time_publi']; 
+    $tmstp = strtotime($d);
+    $dfr = strftime('%A %d %B %Y', $tmstp);
+
+?>
+<main>
+    <section>
+      <br>
+      <br>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1 class="titleA"><?=$articleR[0]['title']; ?></h1>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6  mx-auto">
+                    <img src="<?=$articleR[0]['img']; ?>" class="img-fluid " alt=""><br>
+                    <p>Publié le <?=$dfr ; ?> </p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                    <h3><?=$articleR[0]['description']; ?></h3>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 mx-auto text" >
+                    <?=$articleR[0]['text']; ?>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+    <br>
+</main>
+<?php
+    require_once('../views/templates/b_footer.php');
+?>
