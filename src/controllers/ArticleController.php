4029e2979f28c09a47d5225b7681beccc7637c2c
d1mj0 <d.iosypenko@gmail.com>
2021-10-22 15:25:13 +0200

    b_view_publication.php

--- src/controllers/ArticleController.php
+++ src/controllers/ArticleController.php
@@ -45,9 +45,9 @@ class ArticleController {
         ob_end_flush();
     }
 
-    public function deleteArticle(){
+    public function deleteArticle($id){
         $articleModel = new ArticleModel();
-        $articleModel->delete($this->title, $this->description, $this->text, $this->img);
+        $articleModel->delete($this->title, $this->description, $this->text, $this->img, $id);
         header('Location: b_table_article.php');
         ob_end_flush();
     }
