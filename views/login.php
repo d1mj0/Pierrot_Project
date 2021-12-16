0edfcd84d0587b2e9cc1aee286e35420bc1b5247
Xeuhma! <xhm1@protonmail.com>
2021-12-16 15:17:14 +0100

    hCaptcha login

--- views/login.php
+++ views/login.php
@@ -32,6 +32,7 @@
                                                 <input class="form-control" id="password" type="password" name="password" placeholder="Mot de passe" />
                                                 <label for="password">Mot de passe</label>
                                             </div>
+                                            <div class="h-captcha" data-sitekey="00444aed-7125-4159-bf73-48bcd3055823"></div>
                                             <div class="d-flex align-items-center justify-content-center mt-4 mb-0">
                                                 <button type="submit">Se connecter</button>
                                             </div>
